3261d835151f478e60440a0986ad412e09a6f772
Troy Wu <pightmp@gmail.com>
2018-12-06 10:28:05 +0800

    temp

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,12 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ManifestPlugin = require('webpack-manifest-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: {
     app: './src/scripts/index.js',
     print: './src/scripts/print.js',
   },
+  devtool: 'inline-source-map',
   plugins: [
     new ManifestPlugin(),
     new CleanWebpackPlugin(['dist']),
