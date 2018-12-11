e39a62702925ac43b4d260a9bf17d916021ba235
Troy Wu <pightmp@gmail.com>
2018-12-12 00:28:08 +0800

    feat: add module replacement

--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,29 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ManifestPlugin = require('webpack-manifest-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   mode: 'development',
   entry: {
     app: './src/scripts/index.js',
-    print: './src/scripts/print.js',
   },
   devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+  },
   plugins: [
     new ManifestPlugin(),
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
-      title: 'Output Management',
+      title: 'Hot Module Replacement',
     }),
+    new webpack.HotModuleReplacementPlugin(),
   ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
   },
 };
