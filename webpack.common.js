003952cb54786ed30f255b5795cfa17e6d494335
Troy Wu <pightmp@gmail.com>
2018-12-21 18:14:30 +0800

    production

---
+++ webpack.common.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
+
+
+module.exports = {
+  entry: {
+    app: './src/scripts/index.js',
+  },
+  plugins: [
+    new ManifestPlugin(),
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Production',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+}
