108121eb2074e537914ab70546bb5d4652862973
Troy Wu <pightmp@gmail.com>
2019-02-11 17:51:33 +0800

    final

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,10 @@ const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ManifestPlugin = require('webpack-manifest-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const webpack = require('webpack');
 const WorkboxPlugin = require('workbox-webpack-plugin');
 
@@ -14,18 +17,21 @@ module.exports = {
     new ManifestPlugin(),
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
-      title: 'Progressive Web Application',
+      title: 'New app',
     }),
     new BundleAnalyzerPlugin(),
     new webpack.HashedModuleIdsPlugin(),
-    new webpack.ProvidePlugin({
-      _: 'lodash',
-    }),
     new WorkboxPlugin.GenerateSW({
       // these options encourage the ServiceWorkers to get in there fast
       // and not allow any straggling "old" SWs to hang around
       clientsClaim: true,
       skipWaiting: true,
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: 'css/[name].css',
+      chunkFilename: 'css/[name].css',
     }),
   ],
   module: {
@@ -40,6 +46,17 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader', // translates CSS into CommonJS
+          'postcss-loader',
+          'sass-loader', // compiles Sass to CSS, using Node Sass by default
+        ],
+      },
     ],
   },
   output: {
@@ -48,6 +65,11 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   optimization: {
+    usedExports: true,
+    minimizer: [
+      new OptimizeCssAssetsPlugin({}),
+      new UglifyJsPlugin(),
+    ],
     runtimeChunk: 'single',
     splitChunks: {
       cacheGroups: {
